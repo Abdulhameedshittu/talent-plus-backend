0fa3f8f3705f3324ea6466c93cd7bc032bc85322
abdulhameed <abdulhameedshittu@gmail.com>
2022-11-16 08:53:08 -0800

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/',(req, res) => {
     res.send("TALENT PLUS")
